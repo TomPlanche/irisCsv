24d701e1521a7677925b97ce736672793a9b95b7
Tom Planche <58936594+TomPlanche@users.noreply.github.com>
2020-12-05 13:14:09 +0100

    Update irisCsv.py

--- irisCsv.py
+++ irisCsv.py
@@ -1,100 +1,102 @@
+
 import matplotlib.patches as mpatches
 from math import sqrt
 import matplotlib.pyplot as plt
 from termcolor import colored
 
 
-def faireGraph(fichierCsv):
-    with open(fichierCsv) as fichier:
-        toutesLignes = [ligne.rstrip().rsplit(',') for ligne in fichier.readlines() if ligne != ""]
-        fichier.close()
+def graph(csv_file):
+    with open(csv_file) as file:
+        all_lines = [line.rstrip().rsplit(',') for line in file.readlines() if line != ""]
+        file.close()
 
-    xLabel = toutesLignes[0][0]
-    yLabel = toutesLignes[0][1]
+    x_label = all_lines[0][0]
+    y_label = all_lines[0][1]
 
-    coordX = [float(elem[0]) for elem in toutesLignes[1:]]
-    coordY = [float(elem[1]) for elem in toutesLignes[1:]]
+    x_coord = [float(elem[0]) for elem in all_lines[1:]]
+    y_coord = [float(elem[1]) for elem in all_lines[1:]]
 
-    toutesEspeces = [elem[2] for elem in toutesLignes[1:]]
+    all_species = [elem[2] for elem in all_lines[1:]]
 
-    for i in range(len(coordX)):
-        if toutesEspeces[i] == 'setosa':
-            plt.scatter(coordX[i], coordY[i], c = 'green')
-        elif toutesEspeces[i] == 'virginica':
-            plt.scatter(coordX[i], coordY[i], c = 'red')
+    for i in range(len(x_coord)):
+        if all_species[i] == 'setosa':
+            plt.scatter(x_coord[i], y_coord[i], c = 'green')
+        elif all_species[i] == 'virginica':
+            plt.scatter(x_coord[i], y_coord[i], c = 'red')
         else:
-            plt.scatter(coordX[i], coordY[i], c = 'blue')
+            plt.scatter(x_coord[i], y_coord[i], c = 'blue')
 
-    choixAjtValeur = input("Voulez vous ajouter une fleur au graphique et déterminer son espèce ? (oui/non) ? : ")
+    add_value_choice = input("Would you like to add a flower to the graph and identify it ? (yes/no) : ")
 
-    legendeVerte = mpatches.Patch(color = 'green', label = "Setosa")
-    legendeRouge = mpatches.Patch(color = 'red', label = "Virginica")
-    legendeBleue = mpatches.Patch(color = 'blue', label = "Versicolor")
+    green_legend = mpatches.Patch(color = 'green', label = "Setosa")
+    red_legend = mpatches.Patch(color = 'red', label = "Virginica")
+    blue_legend = mpatches.Patch(color = 'blue', label = "Versicolor")
 
-    if choixAjtValeur in ['oui', 'ui', 'o', 'y', 'yes']:
-        longP = float(input("\nLongueur d'une pétale : "))
+    if add_value_choice in ['ye', 'yes', 'y']:
+        petal_length = float(input("\nLength of a petal : "))
 
-        largP = float(input("Largeur d'une pétale : "))
+        petal_width = float(input("Width of a petal : "))
 
-        ecartT = []
-        for j in range(len(coordX)):
-            ecartT.append((
+        gap_between_flowers = []
+        for j in range(len(x_coord)):
+            gap_between_flowers.append((
                 round(sqrt(
-                        (coordX[j] - longP) ** 2 + (coordY[j] - largP) ** 2
+                        (x_coord[j] - petal_length) ** 2 + (y_coord[j] - petal_width) ** 2
                 ), 2)
                 , j)
             )
 
-        ecartT.sort(key = lambda x: x[0])
+        gap_between_flowers.sort(key = lambda x: x[0])
 
-        typeEcart = []
-        for x in range(len(coordX)):
-            typeEcart.append(toutesLignes[ecartT[x][1]][2])
+        gap_type = []
+        for x in range(len(x_coord)):
+            gap_type.append(all_lines[gap_between_flowers[x][1]][2])
 
-        choixNbVois = int(input(f"\nPour combien de voisin(s) voulez vous que l'on détermine l'espèce ? : ")) - 1
+        number_of_neighbours = int(input(f"\nHow much neighbours do you want the program to "
+                                         f"determind the specie ? : ")) - 1
 
         versi, virgi, seto = 0, 0, 0
 
-        for a in range(choixNbVois):
-            if toutesEspeces[ecartT[a][1]] == 'setosa':
+        for a in range(number_of_neighbours):
+            if all_species[gap_between_flowers[a][1]] == 'setosa':
                 seto += 1
-            elif toutesEspeces[ecartT[a][1]] == 'virginica':
+            elif all_species[gap_between_flowers[a][1]] == 'virginica':
                 virgi += 1
             else:
                 versi += 1
 
         if max(virgi, versi, seto) == versi:
-            print(f"\nVotre trouvaille est surement une iris {colored('versicolor', 'blue')}")
+            print(f"\nYou've probalby found an iris {colored('versicolor', 'blue')}")
 
         elif max(virgi, versi, seto) == seto:
-            print(f"\nVotre trouvaille est surement une iris {colored('setosa', 'green')}")
+            print(f"\nYou've probalby found an iris {colored('setosa', 'green')}")
 
         else:
-            print(f"\nVotre trouvaille est surement une iris {colored('virginica', 'red')}")
+            print(f"\nYou've probalby found an iris {colored('virginica', 'red')}")
 
-        plt.scatter(longP, largP, c = 'k')
+        plt.scatter(petal_length, petal_width, c = 'k')
 
-        legendeInco = mpatches.Patch(color = 'black', label = 'trouvaille')
+        legendeInco = mpatches.Patch(color = 'black', label = 'your_finding')
 
-        plt.legend(handles = [legendeRouge, legendeBleue, legendeVerte, legendeInco])
+        plt.legend(handles = [red_legend, blue_legend, green_legend, legendeInco])
         plt.show()
 
-    elif choixAjtValeur in ['n', 'nn', 'non', 'no']:
+    elif add_value_choice in ['n', 'nn', 'non', 'no']:
 
-        plt.legend(handles = [legendeRouge, legendeBleue, legendeVerte])
-        plt.xlabel(xLabel)
-        plt.ylabel(yLabel)
-        plt.title("Schéma base de données iris")
+        plt.legend(handles = [red_legend, blue_legend, green_legend])
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        plt.title("Iris base model")
 
         plt.show()
 
-        print("Affichage du graphique en cours...\n")
+        print("Graph loading...\n")
 
-    if choixAjtValeur not in ['n', 'nn', 'non', 'oui', 'ui', 'o']:
-        print("\nLa réponse demandée est soit: oui soit: non")
-        faireGraph(fichierCsv)
+    if add_value_choice not in ['n', 'nn', 'non', 'yes', 'y']:
+        print("\nYes or No ?")
+        graph(csv_file)
 
 
-fichier_csv = '/path_to/iris_2D.csv'
+fichier_csv = 'path_to_iris_2D.csv'
 
-faireGraph(fichier_csv)
+graph(fichier_csv)
